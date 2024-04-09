86d7d9a3ac84c5e24de4341ed60c96d6d8de92a1
takokat <danobaba@protonmail.com>
2024-04-08 17:57:50 -0700

    Add in age, gender, race, fungi experience, blockchain course boolean to the survey form

--- src/components/forms/survey/SurveyForm.tsx
+++ src/components/forms/survey/SurveyForm.tsx
@@ -12,6 +12,7 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
+  FormDescription,
 } from "@/components/ui/form";
 
 import {
@@ -21,6 +22,9 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+
+import { Switch } from "@/components/ui/switch";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -52,14 +56,18 @@ export const SurveyForm = ({
   const form = useForm<FormSchema>({
     resolver: zodResolver(schema),
     defaultValues: {
+      age: undefined,
       gender: undefined,
+      race_ethnicity: undefined,
+      fungi_exp: undefined,
+      blockchain_course: false,
       address: "",
     },
   });
 
   const formRef = useRef<HTMLFormElement>(null);
 
-  https: return (
+  return (
     <Form {...form}>
       <div>{state?.message}</div>
       <form
@@ -70,26 +78,193 @@ export const SurveyForm = ({
       >
         <FormField
           control={form.control}
-          name="gender"
+          name="age"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Gender</FormLabel>
-              <Select onValueChange={field.onChange}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a gender" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="male">Male</SelectItem>
-                  <SelectItem value="female">Female</SelectItem>
-                  <SelectItem value="other">Other</SelectItem>
-                </SelectContent>
-              </Select>
+              <FormLabel>Age</FormLabel>
+              <FormControl>
+                <Input
+                  className="w-36"
+                  type="number"
+                  placeholder="Enter age..."
+                  {...field}
+                />
+              </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
+        <FormField
+          control={form.control}
+          name="gender"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Select your gender...</FormLabel>
+              <RadioGroup
+                onValueChange={field.onChange}
+                defaultValue={field.value}
+                className="flex flex-col space-y-1"
+              >
+                <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="male" />
+                  </FormControl>
+                  <FormLabel className="font-normal">Male</FormLabel>
+                </FormItem>
+                <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="female" />
+                  </FormControl>
+                  <FormLabel className="font-normal">Female</FormLabel>
+                </FormItem>
+                <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="other" />
+                  </FormControl>
+                  <FormLabel className="font-normal">Other</FormLabel>
+                </FormItem>
+              </RadioGroup>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="race_ethnicity"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Select your race/ethnicity...</FormLabel>
+              <RadioGroup
+                onValueChange={field.onChange}
+                defaultValue={field.value}
+                className="flex flex-col space-y-1"
+              >
+                <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="native" />
+                  </FormControl>
+                  <FormLabel className="font-normal">
+                    American Indian or Alaska Native
+                  </FormLabel>
+                </FormItem>
+                <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="asian" />
+                  </FormControl>
+                  <FormLabel className="font-normal">Asian</FormLabel>
+                </FormItem>
+                <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="black" />
+                  </FormControl>
+                  <FormLabel className="font-normal">
+                    Black or African American
+                  </FormLabel>
+                </FormItem>
+                <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="pacific" />
+                  </FormControl>
+                  <FormLabel className="font-normal">
+                    Native Hawaiian or Other Pacific Islander
+                  </FormLabel>
+                </FormItem>
+                <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="latino" />
+                  </FormControl>
+                  <FormLabel className="font-normal">
+                    Hispanic or Latino
+                  </FormLabel>
+                </FormItem>
+                <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="white" />
+                  </FormControl>
+                  <FormLabel className="font-normal">White</FormLabel>
+                </FormItem>
+                <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="mixed" />
+                  </FormControl>
+                  <FormLabel className="font-normal">Mixed</FormLabel>
+                </FormItem>
+                <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="no" />
+                  </FormControl>
+                  <FormLabel className="font-normal">No Response</FormLabel>
+                </FormItem>
+              </RadioGroup>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="space-y-10">
+          <FormField
+            control={form.control}
+            name="fungi_exp"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Let us know your experience with fungi...</FormLabel>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col space-y-1"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="beginner" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Beginner</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="intermediate" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Intermediate</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="advanced" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Advanced</FormLabel>
+                  </FormItem>
+                </RadioGroup>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="">
+            <FormField
+              control={form.control}
+              name="blockchain_course"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                  <div className="space-y-0.5">
+                    <FormLabel className="text-base">
+                      Blockchain course
+                    </FormLabel>
+                    <FormDescription>
+                      Will you be taking our Algorand course?
+                    </FormDescription>
+                  </div>
+                  <FormControl>
+                    <div className="flex flex-col space-y-2">
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                      <span className="flex justify-center items-center text-sm">
+                        {field.value ? "Yes" : "No"}
+                      </span>
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
         <FormField
           control={form.control}
           name="address"
--- src/utils/zod/surveyFormSchema.tsx
+++ src/utils/zod/surveyFormSchema.tsx
@@ -3,8 +3,38 @@ import { z } from "zod";
 import { validateAlgorandAddress } from "@/app/api/validate/url/validateAlgorandAddress";
 
 export const schema = z.object({
-  gender: z.enum(["male", "female", "other"]),
-  address: z.string().trim().refine(validateAlgorandAddress, {
-    message: "Invalid NFD or Algorand Address",
+  age: z.coerce
+    .number({
+      required_error: "Age is required",
+      invalid_type_error: "Age must be a number",
+    })
+    .int()
+    .gte(8, {
+      message: "Must be at least 8 years old",
+    })
+    .lte(122, {
+      message: "Humans can't live this long yet! ",
+    }),
+  gender: z.enum(["male", "female", "other"], {
+    required_error: "You need to select a gender.",
   }),
+  race_ethnicity: z.enum(
+    ["native", "asian", "black", "pacific", "latino", "white", "mixed", "no"],
+    {
+      required_error: "You need to select a race/ethnicity.",
+    }
+  ),
+  fungi_exp: z.enum(["beginner", "intermediate", "advanced"], {
+    required_error: "You need to select an experience.",
+  }),
+  blockchain_course: z.boolean().default(true),
+  address: z
+    .string()
+    .max(58, {
+      message: "Must not exceed 58 characters",
+    })
+    .trim()
+    .refine(validateAlgorandAddress, {
+      message: "Invalid NFD or Algorand Address",
+    }),
 });
