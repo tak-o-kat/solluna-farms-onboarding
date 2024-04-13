7cc8f587e3e20b30cffbabdd798e7b13deacb011
takokat <danobaba@protonmail.com>
2024-04-13 09:37:53 -0700

    Update to padding on mobile views for error pages

--- src/app/not-found/survey/error/page.tsx
+++ src/app/not-found/survey/error/page.tsx
@@ -1,8 +1,8 @@
 export default function error() {
   return (
-    <div className="min-h-screen flex flex-row justify-center items-center p-24">
+    <div className="min-h-screen flex flex-row justify-center items-center p-4 sm:p-24">
       <h1 className="flex flex-row justify-center items-center">
-        500 - Server-side error occurred
+        500 | Server-side error occurred
       </h1>
     </div>
   );
--- src/app/not-found/survey/invalid/page.tsx
+++ src/app/not-found/survey/invalid/page.tsx
@@ -1,8 +1,8 @@
 export default function invalid() {
   return (
-    <div className="min-h-screen flex flex-row justify-center items-center p-24">
+    <div className="min-h-screen flex flex-row justify-center items-center p-3 sm:p-24">
       <h1 className="flex flex-row justify-center items-center">
-        403 | The Url ID was either incorrect or no longer valid!
+        403 | Invalid Url ID!
       </h1>
     </div>
   );
