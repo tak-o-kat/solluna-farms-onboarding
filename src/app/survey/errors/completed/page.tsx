ae26b2345259ea2b8d9c6404c2e6ac16a617f31c
takokat <danobaba@protonmail.com>
2024-04-15 23:59:24 -0700

    Fixed vertical margins on error pages

--- src/app/survey/errors/completed/page.tsx
+++ src/app/survey/errors/completed/page.tsx
@@ -1,6 +1,6 @@
 export default function invalid() {
   return (
-    <div className="min-h-screen flex flex-row justify-center items-center -mt-20 p-3 sm:p-24">
+    <div className="min-h-screen flex flex-row justify-center items-center -mt-24 p-3 sm:p-24">
       <span className="flex text-2xl gap-2 p-3 border-r-2 ">403</span>
       <span className="flex text-sm sm:text-base p-3 ">
         Survey ID already submitted!
