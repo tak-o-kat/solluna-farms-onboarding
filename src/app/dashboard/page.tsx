5a58868add5319a7a5e4406fa6db8ea2ec092d56
takokat <danobaba@protonmail.com>
2024-04-18 15:28:48 -0700

    Add in custom card template to dashboard home

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,74 @@
+import Link from "next/link";
+import CustomCard from "@/components/dashboard/home/CustomCards";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar";
+
+import {
+  CardTitle,
+  CardHeader,
+  CardContent,
+  Card,
+  CardDescription,
+} from "@/components/ui/card";
+import {
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableCell,
+  TableBody,
+  Table,
+} from "@/components/ui/table";
+
+import {
+  LinkIcon,
+  FilePen,
+  WalletMinimal,
+  ArrowUpRightIcon,
+} from "lucide-react";
 
 export default async function DashboardPage() {
   const { isAuthenticated } = getKindeServerSession();
   const isLoggedIn = await isAuthenticated();
 
   return (
-    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      dashbboard
+    <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+        <CustomCard
+          chunk="0"
+          title="Urls Generated"
+          icon={LinkIcon}
+          amount={20}
+          percentage={100}
+          unit="month"
+        />
+        <CustomCard
+          chunk="0"
+          title="Urls Sent"
+          icon={LinkIcon}
+          amount={16}
+          percentage={100}
+          unit="month"
+        />
+        <CustomCard
+          chunk="1"
+          title="Surveys Completed"
+          icon={FilePen}
+          amount={14}
+          percentage={100}
+          unit="month"
+        />
+        <CustomCard
+          chunk="2"
+          title="Total Accounts Tracked"
+          icon={WalletMinimal}
+          amount={5}
+          percentage={100}
+          unit="month"
+        />
+      </div>
     </main>
   );
 }
