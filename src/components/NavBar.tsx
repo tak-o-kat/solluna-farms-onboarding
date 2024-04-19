5f04f4c55f135a9b2230ee6b363b8873595dbb7c
takokat <danobaba@protonmail.com>
2024-04-19 11:53:36 -0700

    Adjust Navbar padding on logo in mobile view

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ export default function NavBar() {
   return (
     <div className="flex flex-row justify-center h-[5rem] w-full border-b">
       <nav className="flex flex-row items-center max-w-5xl w-full px-3">
-        <div className="basis-3/4 flex flex-row gap-3 justify-start items-center w-full">
+        <div className="basis-3/4 flex flex-row gap-2 ps-2 justify-start items-center w-full">
           <Image
             src={"/favicon.svg"}
             alt={"Solluna Farms Logo"}
