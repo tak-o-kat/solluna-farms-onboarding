2444e5e6183566fa5d4ba6893d85ea0005d0b52c
takokat <danobaba@protonmail.com>
2024-04-19 20:31:54 -0700

    Remove cookies set test from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,8 @@
-"use client";
-import { useEffect, useState } from "react";
-import { setStatusColumnInCookie } from "@/app/actions/surveyTableActions";
-
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  useEffect(() => {
-    async function fetchData() {
-      await setStatusColumnInCookie("home");
-    }
-    if (true) {
-      fetchData();
-    }
-  }, []);
-
   return (
     <main className="w-screen flex flex-col justify-center items-center text-md sm:text-xl p-24">
       <div className="flex flex-col justify-center items-center">
