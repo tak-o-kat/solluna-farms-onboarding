9430efff7581224a04e0b77044ad1fd5ed270fa7
takokat <danobaba@protonmail.com>
2024-05-21 15:43:49 -0700

    Update deprecated packages and changes dashboard menu font size

--- src/components/dashboard/DashboardMenu.tsx
+++ src/components/dashboard/DashboardMenu.tsx
@@ -22,7 +22,7 @@ export default function DashboardMenu() {
           </Link>
         </div>
         <div className="flex-1">
-          <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+          <nav className="grid items-start px-2 text-md font-medium lg:px-4">
             <Link
               href="/dashboard"
               className={`${
